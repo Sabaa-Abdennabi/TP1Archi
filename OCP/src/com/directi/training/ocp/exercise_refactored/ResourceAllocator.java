825a1a4961fb679707f9e4fd3aa74dbefe3b88d4
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2025-02-07 11:09:34 +0100

    added try catch

--- OCP/src/com/directi/training/ocp/exercise_refactored/ResourceAllocator.java
+++ OCP/src/com/directi/training/ocp/exercise_refactored/ResourceAllocator.java
@@ -1,18 +1,20 @@
 package com.directi.training.ocp.exercise_refactored;
 
-public class ResourceAllocator
-{
-    //private static final int INVALID_RESOURCE_ID = -1;
+public class ResourceAllocator {
+    // private static final int INVALID_RESOURCE_ID = -1;
 
-    public int allocate(Resource resource)
-    {
-        int resourceId = resource.findFreeSlot();
-        resource.markSlotBusy(resourceId);
-        return resourceId;
+    public int allocate(Resource resource) {
+        try {
+            int resourceId = resource.findFreeSlot();
+            resource.markSlotBusy(resourceId);
+            return resourceId;
+        } catch (Exception e) {
+            System.out.println("ERROR: Attempted to allocate invalid resource");
+            return -1;
+        }
     }
 
-    public void free(Resource resource, int resourceId)
-    {
+    public void free(Resource resource, int resourceId) {
         resource.markSlotFree(resourceId);
     }
 }
